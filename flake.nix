be0e2abf36b0831bd1d6e16eb1137d9e478c2173
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-05-09 00:37:33 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,10 @@
 {
-  inputs = {
-    "unstable".url = "github:NixOS/nixpkgs/nixos-unstable";
-    "24.11".url = "github:NixOS/nixpkgs/nixos-24.11";
-    "24.05".url = "github:NixOS/nixpkgs/nixos-24.05";
-  };
-  
-  outputs = { self, inputs }: {
+  outputs = { self, nixpkgs }: {
     __functor = self: {
       arch ? "x86_64",
-      version ? "unstable",
       modules ? []
     }: let
       system = "${arch}-linux";
-      nixpkgs = import inputs.${version} { inherit system; };
       lib = nixpkgs.lib;
     in lib.nixosSystem {
       inherit system;
